58196fb975ccc1a6a90cfd7679f86dcf60598032
Jay <jayhpaq@gmail.com>
2018-05-21 16:10:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ function theBeatlesPlay(musicians, instruments) {
   var allPlayers = [];
   for (var i=0; i < players.length; i++) {
  allPlayers.push(players[i] + " plays " + instruments[i]);
- 
 }
