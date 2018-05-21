c2f4ff36093cec6f032c3bc9111201afaa34561a
Jay <jayhpaq@gmail.com>
2018-05-21 16:17:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array
+}function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+  
